4b9691af5830a92aec5afbcbe29624255f00d0c8
Carlos Arias <73307699+Carlos-Arias-Galindo@users.noreply.github.com>
2022-01-06 11:45:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ ctx2.shadowBlur = 0;
 ctx2.shadowColor = 'white';
 */
 class Symbol {
-    constructor(x, y, fontSize, canvasHeight) {
+    constructor(x, y, fontSize, canvasHeight){
         this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         this.x = x;
         this.y = y;
@@ -23,41 +23,39 @@ class Symbol {
         this.canvasHeight = canvasHeight;
         //this.color = 'hsl(' + this.x * 3+ ', 100%, 50%)';
     }
-    draw(context, context2) {
+    draw(context, context2){
         //context.font = this.fontSize + 'px monospace';
         this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
         //context.fillStyle = this.color;
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
         context2.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.97) {
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.97){
             this.y = 0;
-        } else {
+        }
+        else {
             this.y += 0.9;
         }
     }
 }
 
 class Effect {
-    constructor(canvasWidth, canvasHeight) {
+    constructor(canvasWidth, canvasHeight){
         this.fontSize = 16;
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
-        this.columns = this.canvasWidth / this.fontSize;
+        this.columns = this.canvasWidth/this.fontSize;
         this.symbols = [];
         this.#initialize();
     }
-
-
-   #initilize() {
+    #initialize(){
         for (let i = 0; i < this.columns; i++) {
             this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
         }
     }
-
-    resize(width, height) {
+    resize(width, height){
         this.canvasWidth = width;
         this.canvasHeight = height;
-        this.columns = this.canvasWidth / this.fontSize;
+        this.columns = this.canvasWidth/this.fontSize;
         this.symbols = [];
         this.#initialize();
     }
@@ -65,13 +63,13 @@ class Effect {
 const effect = new Effect(canvas.width, canvas.height);
 let lastTime = 0;
 const fps = 26;
-const nextFrame = 1000 / fps;
+const nextFrame = 1000/fps;
 let timer = 0;
 
-function animate(timeStamp) {
+function animate(timeStamp){
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    if (timer > nextFrame) {
+    if (timer > nextFrame){
         ctx.textAlign = "center";
         ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -88,12 +86,12 @@ function animate(timeStamp) {
         timer = 0;
     } else {
         timer += deltaTime;
-    }
+    } 
     requestAnimationFrame(animate);
 }
 animate(0);
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas2.width = window.innerWidth;
